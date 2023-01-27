f516c1bef4a3f5506a9998db55411c21755da375
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2023-01-26 20:39:02 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+ *  Author: Ruben Villagrana
+ *  Course: COMP-003A
+ *  Purpose: Replicate output
+ */  
+namespace COMP_3A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a string");
+            Console.WriteLine("Ruben Villagrana");
+
+        }
+    }
+}
